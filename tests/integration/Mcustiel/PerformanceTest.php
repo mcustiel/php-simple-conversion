5e3956e68c4b3cb892cf91722faa6ff234fef16c
Mariano Custiel <mcustiel@gmail.com>
2014-12-07 18:14:20 -0200

    Performance tests

---
+++ tests/integration/Mcustiel/PerformanceTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Integration;
+
+use Mcustiel\PhpSimpleConversion\ConversionService;
+use Mcustiel\PhpSimpleConversion\ConverterBuilder;
+use Fixtures\AToBConverter;
+use Fixtures\A;
+use Fixtures\B;
+
+class PerformanceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPerformance()
+    {
+        $service = new ConversionService();
+        $builder = ConverterBuilder::get()
+            ->from(A::class)
+            ->to(B::class)
+            ->withImplementation(AToBConverter::class);
+        $service->registerConverter($builder);
+
+        $a = new A(1, json_encode(
+            array(
+                'firstName' => 'john',
+                'lastName' => 'doe',
+                'age' => 30
+        )));
+
+        foreach ([5000, 15000, 25000, 50000] as $cycles) {
+            $start = microtime(true);
+            for ($i = $cycles; $i > 0; $i--) {
+                $b = $service->convert($a, B::class);
+            }
+            echo "\n{$cycles} cycles executed in " . (microtime(true) - $start) . " microseconds\n";
+        }
+    }
+}
